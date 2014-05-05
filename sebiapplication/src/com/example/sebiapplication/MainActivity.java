d75d5d184e57545ac041236178f27a5d8c0db9ce
AnujKosambi <anujkosambi@gmail.com>
2014-05-05 20:02:31 +0530

    master

---
+++ sebiapplication/src/com/example/sebiapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sebiapplication;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends SherlockFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+   
+    
+}
