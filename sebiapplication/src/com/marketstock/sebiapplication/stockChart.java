99cf2770b923d36a52714d8a469f8c65c24075e1
AnujKosambi <anujkosambi@gmail.com>
2014-05-27 03:17:21 +0530

    Graph StartUP

--- sebiapplication/src/com/marketstock/sebiapplication/stockChart.java
+++ sebiapplication/src/com/marketstock/sebiapplication/stockChart.java
@@ -1,12 +1,32 @@
 package com.marketstock.sebiapplication;
 
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+
+import android.R.string;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.example.sebiapplication.R;
+import com.marketstock.helper.GraphViewData;
+import com.marketstock.sebiapplication.models.Stock;
+import com.jjoe64.graphview.CustomLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphViewDataInterface;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.GraphViewSeries.GraphViewSeriesStyle;
+import com.jjoe64.graphview.LineGraphView;
+import com.jjoe64.graphview.ValueDependentColor;
 
 public class stockChart extends SherlockFragment{
 
@@ -15,6 +35,72 @@ public class stockChart extends SherlockFragment{
             Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.stock_chart, container, false);
+        GraphViewSeriesStyle seriesStyle = new GraphViewSeriesStyle();
+        
+	    GraphViewSeries series = new GraphViewSeries("Graph_Title", seriesStyle, new GraphViewData[] {});
+       	LineGraphView graphView = new LineGraphView(this.getActivity(), "Quote");
+       	graphView.addSeries(series); // data
+     	graphView.setViewPort(2, 6);
+       	graphView.setScrollable(true);
+       	graphView.setScalable(true);
+       	graphView.getGraphViewStyle().setVerticalLabelsWidth(30);
+      // 	graphView.setVerticalLabels(new String[] {"high", "middle", "low"});
+       	graphView.getGraphViewStyle().setGridColor(Color.GRAY);
+       	graphView.getGraphViewStyle().setHorizontalLabelsColor(Color.BLACK);
+       	graphView.getGraphViewStyle().setVerticalLabelsColor(Color.BLACK);
+       	graphView.getGraphViewStyle().setTextSize(10);
+     	graphView.getGraphViewStyle().setNumHorizontalLabels(5);
+       //	graphView.getGraphViewStyle().setNumVerticalLabels(4);
+       //	graphView.getGraphViewStyle().setVerticalLabelsWidth(20);
+       //	graphView.getGraphViewStyle().
+       	((LineGraphView) graphView).setDrawBackground(true);
+       	//graphView.
+       	
+       	
+       	graphView.setBackgroundColor(Color.rgb(194, 223, 255));
+        graphView.setDrawDataPoints(true);
+       	graphView.setDataPointsRadius(2f);
+  
+       	Date fromDate=new Date(1,1,2014);
+       	Date toDate=new Date(31,12,2014);
+   //    	graphView.setHorizontalLabels(new String[] {fromDate.toString(),toDate.toString()});
+       	ArrayList<String> dateList=new ArrayList<String>();
+       	int[] monthMarker=new int[12];
+        for(int i=0;i<360;i++)
+        {
+        	@SuppressWarnings("deprecation")
+  			Date date=new Date(2014,i/30+1, i%30+1);
+          	monthMarker[i/30]=1;
+        	Random random=new Random();
+        	double openPrice= random.nextDouble();
+        	double closePrice= random.nextDouble();
+        	double highPrice= random.nextDouble();
+        	double lowPrice= random.nextDouble();
+        	double volume= random.nextDouble();        			
+        	Stock stock=new Stock(date,openPrice,closePrice,highPrice,lowPrice,volume);
+        	series.appendData(new GraphViewData(i*1.0,closePrice), true);
+            if(i%30==0)
+        	dateList.add(date.toString());
+        }
+
+        String[] stockArr = new String[dateList.size()];
+        stockArr = dateList.toArray(stockArr);
+        
+    	//graphView.setHorizontalLabels(stockArr);
+        graphView.setCustomLabelFormatter(new CustomLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    // Assuming only 7 total values here
+                    return ""+value;
+                } else
+                    return String.format("%.2f", value);
+            }
+        });
+//        graphView.getGraphViewStyle().setNumHorizontalLabels(3);
+    	LinearLayout layout = (LinearLayout)rootView.findViewById(R.id.ChartLayout);
+    	layout.addView(graphView);
+    	
         
         return rootView;
     }
